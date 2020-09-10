4463422958160ea112fbe954fda813d76eec7310
sg550550 <605549855@qq.com>
2020-09-10 12:44:33 +0800

    Update conf.js

--- conf.js
+++ conf.js
@@ -13,7 +13,7 @@ jsproxy_config({
   // 节点配置
   node_map: {
     'demo-hk': {
-      label: '演示服务-香港节点',
+      label: '演示服务-香港',
       lines: {
         // 主机:权重
         'node-aliyun-hk-1.etherdream.com:8443': 1,
